93746db97c7053bc379ee31252cd2bb09c6ffbca
Daniel Eichhorn <dani.eichhorn@squix.ch>
2020-11-17 21:13:37 +0100

    FX library

--- src/main.cpp
+++ src/main.cpp
@@ -1,4 +1,12 @@
 #include "BluetoothA2DPSink.h"
+#include <arduinoFFT.h> 
+
+#define NUM_BANDS  8
+#define READ_DELAY 50
+#define USE_RANDOM_DATA false
+
+#include <WS2812FX.h>        //https://github.com/kitesurfer1404/WS2812FX
+#include "custom/VUMeter.h"  //^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^
 
 BluetoothA2DPSink a2dp_sink;
 
@@ -7,28 +15,32 @@ BluetoothA2DPSink a2dp_sink;
 #define I2S_LRC       22
 #define MODE_PIN       33
 
-static int16_t sample_l_int = 0;
-static int16_t sample_r_int = 0;
-static float sample_l_float = 0.0f;
-static float sample_r_float = 0.0f;
-static float in = 0.0f;
-static float out = 0.0f;
-static float out2 = 0.0f;
-static float biquad1_z1 = 0.0f;
-static float biquad1_z2 = 0.0f;
-static float biquad2_z1 = 0.0f;
-static float biquad2_z2 = 0.0f;
-static float biquad3_z1 = 0.0f;
-static float biquad3_z2 = 0.0f;
-static float biquad4_z1 = 0.0f;
-static float biquad4_z2 = 0.0f;
-static float biquad5_z1 = 0.0f;
-static float biquad5_z2 = 0.0f;
-uint32_t s_pkt_cnt = 0;
+arduinoFFT FFT = arduinoFFT();
+
+#define SAMPLES 512              //Must be a power of 2
+#define SAMPLING_FREQUENCY 40000 //Hz, must be 40000 or less due to ADC conversion time. Determines maximum frequency that can be analysed by the FFT.
+
+// LED Settings
+#define LED_COUNT 64
+#define LED_PIN     32
+
+
+WS2812FX ws2812fx = WS2812FX(LED_COUNT, LED_PIN, NEO_GRB + NEO_KHZ800);
+
 
 bool isFilterActivated = false;
 uint32_t lastButtonPressed = 0;
 int pushButton = 39;
+
+int amplitude = 100;
+unsigned int sampling_period_us;
+unsigned long microseconds;
+byte peak[] = {0, 0, 0, 0, 0, 0, 0, 0};
+double vReal[SAMPLES];
+double vImag[SAMPLES];
+unsigned long newTime, oldTime;
+bool isSampleReady = false;
+bool isSampleProcessed = true;
 
 static const i2s_pin_config_t pin_config = {
     .bck_io_num = I2S_BCLK,
@@ -37,79 +49,85 @@ static const i2s_pin_config_t pin_config = {
     .data_in_num = I2S_PIN_NO_CHANGE
 };
 
-void audio_data_callback(const uint8_t *data, uint32_t len) {
-  if (isFilterActivated) {
-    size_t bytes_written;
-    uint8_t *mydat = (uint8_t*) malloc(len);
-    if(mydat == NULL)
-    {
-        ESP_LOGI(BT_AV_TAG, "Not enough memory available");
-    }
-    else
-    {
-        for (uint32_t i = 0; i < len; i+=4)
-        {
-            sample_l_int = (int16_t)((*(data + i + 1) << 8) | *(data + i));
-            sample_r_int = (int16_t)((*(data + i + 3) << 8) | *(data + i +2));
-            sample_l_float = (float)sample_l_int / 0x8000;
-            sample_r_float = (float)sample_r_float / 0x8000;
-            in = (sample_l_float + sample_r_float) / 2.0f;
-            //Audio processing Left Channel
-            //HP 130Hz
-            out = in * (0.9869865272227986f) + biquad1_z1;        //out = in * a0 + z1;
-            biquad1_z1 = in * (-1.9739730544455971f) + biquad1_z2 - (-1.9738037472862642f) * out;    //z1 = in * a1 + z2 - b1 * out;
-            biquad1_z2 = in * (0.9869865272227986f) - (0.9741423616049301f) * out;        //z2 = in * a2 - b2 * out;
-            //Peak Fc 10095, Q 0.478, 6dB Gain
-            out2 = out * (1.5066355805385152f) + biquad5_z1;        //out = in * a0 + z1;
-            biquad5_z1 = out * ( -0.12972459720746804f) + biquad5_z2 - (-0.12972459720746804f) * out2;    //z1 = in * a1 + z2 - b1 * out;
-            biquad5_z2 = out * (-0.5247301530319133f) - (-0.018094572493397725f) * out2;        //z2 = in * a2 - b2 * out;
-            out = out2 * 0.4f;
-            //End of audio processing
-            int32_t a;
-            a = floor(32768 * out);
-            if (a>32767) {a = 32767;}
-            if (a<-32768) {a = -32768;}
-            sample_l_int = (int16_t)a;
-            *(mydat + i + 1) = (uint8_t) ((sample_l_int >> 8) & 0xFF);
-            *(mydat + i) = (uint8_t) (0xFF & sample_l_int);
-            //end of left Channel
-            
-            //Audio processing Right Channel
-            //HP 50Hz 1 Q=0.54119610
-            out = in * (0.993448957683901f) + biquad3_z1;        //out = in * a0 + z1;
-            biquad3_z1 = in * (-1.986897915367802f) + biquad3_z2 - (-1.9868727071697347f) * out;    //z1 = in * a1 + z2 - b1 * out;
-            biquad3_z2 = in * (0.993448957683901f) - (0.9869231235658689f) * out;        //z2 = in * a2 - b2 * out;
-            //HP50Hz 2 Q=1.306563
-            out2 = out * ( 0.9972686246697485f) + biquad4_z1;        //out = in * a0 + z1;
-            biquad4_z1 = out * (-1.994537249339497f) + biquad4_z2 - (-1.9945119442195687f) * out2;    //z1 = in * a1 + z2 - b1 * out;
-            biquad4_z2 = out * (0.9972686246697485f) - (0.9945625544594253f) * out2;        //z2 = in * a2 - b2 * out;
-            //LP 130Hz
-            out = out2 * (0.00008465357966651423f) + biquad2_z1;        //out = in * a0 + z1;
-            biquad2_z1 = out2 * (0.00016930715933302846f) + biquad2_z2 - (-1.9738037472862642f) * out;    //z1 = in * a1 + z2 - b1 * out;
-            biquad2_z2 = out2 * (0.00008465357966651423f) - (0.9741423616049301f) * out;        //z2 = in * a2 - b2 * out;
-            out = out * 1.0f;
-            //End of audio processing
-            a = floor(32768 * out);
-            if (a>32767) {a = 32767;}
-            if (a<-32768) {a = -32768;}
-            sample_r_int = (int16_t)a;
-            *(mydat + i + 3) = (uint8_t) ((sample_r_int >> 8) & 0xFF);
-            *(mydat + i + 2) = (uint8_t) (0xFF & sample_r_int);
-            //end of right Channel            
+uint32_t lastCalc = 0;
+
+void displayBand(int band, int dsize) {
+  int dmax = amplitude;
+  if (dsize > dmax)
+    dsize = dmax;
+  if (dsize > peak[band])
+  {
+    peak[band] = dsize;
+  }
+}
+
+void renderFFT(void * parameter){
+  for(;;){ // infinite loop
+    if (isSampleReady) {
+      isSampleProcessed = false;
+      FFT.Windowing(vReal, SAMPLES, FFT_WIN_TYP_HAMMING, FFT_FORWARD);
+      FFT.Compute(vReal, vImag, SAMPLES, FFT_FORWARD);
+      FFT.ComplexToMagnitude(vReal, vImag, SAMPLES);
+      for (int i = 2; i < (SAMPLES / 2); i++)
+      { // Don't use sample 0 and only first SAMPLES/2 are usable. Each array eleement represents a frequency and its value the amplitude.
+        if (vReal[i] > 2000)
+        { // Add a crude noise filter, 10 x amplitude or more
+          if (i <= 2)
+            displayBand(0, (int)vReal[i] / amplitude); // 125Hz
+          if (i > 3 && i <= 5)
+            displayBand(1, (int)vReal[i] / amplitude); // 250Hz
+          if (i > 5 && i <= 7)
+            displayBand(2, (int)vReal[i] / amplitude); // 500Hz
+          if (i > 7 && i <= 15)
+            displayBand(3, (int)vReal[i] / amplitude); // 1000Hz
+          if (i > 15 && i <= 30)
+            displayBand(4, (int)vReal[i] / amplitude); // 2000Hz
+          if (i > 30 && i <= 53)
+            displayBand(5, (int)vReal[i] / amplitude); // 4000Hz
+          if (i > 53 && i <= 200)
+            displayBand(6, (int)vReal[i] / amplitude); // 8000Hz
+          if (i > 200)
+            displayBand(7, (int)vReal[i] / amplitude); // 16000Hz
         }
-        a2dp_sink.audio_data_callback(mydat, len);
-        //i2s_write(0, mydat, len, &bytes_written, portMAX_DELAY);
-        free (mydat);
+      }
+      if (millis() % 4 == 0)
+      {
+        for (byte band = 0; band < NUM_BANDS; band++)
+        {
+          if (peak[band] > 0)
+            peak[band] /= 2;
+        }
+      } // Decay the peak
+      for (byte band = 0; band < NUM_BANDS; band++)
+      {
+        uint16_t value = peak[band];
+        vuMeterBands[band] = value < 1 ? 0 : map(value, 1, amplitude, 0, 255);
+        Serial.print(value);
+        Serial.print("\t");
+      }
+      Serial.println();
+
+      //ws2812fx.service();
+      isSampleProcessed = true;
+      isSampleReady = false;
     }
-    } else {
-      a2dp_sink.audio_data_callback(data, len);
-    }
-    if (++s_pkt_cnt % 100 == 0) {
-        ESP_LOGI(BT_AV_TAG, "Audio packet count %u", s_pkt_cnt);
-        ESP_LOGI(BT_AV_TAG, "Process task core: %u\n", xPortGetCoreID());
+    log_d("Semaphores: %d, %d", isSampleReady, isSampleProcessed);
+  }
+}
+
+void audio_data_callback(const uint8_t *data, uint32_t len) {
+  if (isFilterActivated && isSampleProcessed) {
+    isSampleReady = false;
+    for (int i = 0; i < SAMPLES; i++){
+      vReal[i] = data[i]; // A conversion takes about 1mS on an ESP8266
+      vImag[i] = 0;
     }
 
-    
+    lastCalc = millis();
+    isSampleReady = true;
+
+  }
+  a2dp_sink.audio_data_callback(data, len);
 }
 
 void onDataReceived() {
@@ -120,16 +138,42 @@ void setup() {
     pinMode(MODE_PIN, OUTPUT);
     pinMode(pushButton, INPUT);
     digitalWrite(MODE_PIN, HIGH);
+
+    xTaskCreatePinnedToCore(
+      renderFFT,      // Function that should be called
+      "Render the FFT",    // Name of the task (for debugging)
+      2000,               // Stack size (bytes)
+      NULL,               // Parameter to pass
+      1,                  // Task priority
+      NULL,               // Task handle
+      1          // Core you want to run the task on (0 or 1)
+    );
+
     a2dp_sink.set_pin_config(pin_config);
-    a2dp_sink.start("Icon64");
+    a2dp_sink.start("ThingPulse-Icon64");
     //a2dp_sink.set_on_data_received(onDataReceived);
     esp_a2d_sink_register_data_callback(audio_data_callback);
+
+    ws2812fx.init();
+    ws2812fx.setBrightness(32);
+
+    // setup the custom effect
+    //uint32_t colors[] = {GREEN, YELLOW, RED};
+    //uint8_t vuMeterMode = ws2812fx.setCustomMode(F("VU Meter"), vuMeter);
+    //ws2812fx.setSegment(0, 0, LED_COUNT-1, vuMeterMode, colors, READ_DELAY, NO_OPTIONS);
+
+    ws2812fx.start();
 }
+
+uint8_t counter = 0;
 
 void loop() {
   int buttonState = digitalRead(pushButton);
   if (!buttonState && millis() - lastButtonPressed > 1000) {
     isFilterActivated = !isFilterActivated;
     lastButtonPressed = millis();
+    counter++;
+    ws2812fx.setMode(counter % 60);
   }
+  ws2812fx.service();
 }
